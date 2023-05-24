5b506d089951bc887ff625b7c34fb5c5f81f75ef
estherrolf <esther_rolf@berkeley.edu>
2023-05-24 06:38:10 -0700

    updating config though i think we just use it in 5_recalibration

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 
-sim_dir = '/data/mosaiks/simulations_normalized/'
+sim_dir = '/data/mosaiks/replication/simulations/'
 FEATURES_FOLDER = '/data/mosaiks/replication/features/'
 FEATURES_FOLDER_1_TILE = '/data/mosaiks/replication/features_1_tile_per_region/'
+SURVEY_FOLDER = '/data/mosaiks/replciation/surveys/'
 
 dataset_keys = {
     'us' : {'FEATURES_FNAME' : FEATURES_FOLDER+'/mosaiks_features_by_puma_us.csv',
-            'LABELS_FNAME' : '/data/mosaiks/surveys/us/groundtruth_by_puma_2019.csv',
+            'LABELS_FNAME' : SURVEY_FOLDER+'us/groundtruth_by_puma_2019.csv',
             'INDIVIDUAL_FNAME': '/data/mosaiks/surveys/us/individual_level_data.csv',
             'MERGE_KEYS' : ['PUMA', 'State'],
             'SPLIT_KEYS' : {'spatial':'State', 'no_spatial':'PUMA'},
@@ -18,8 +19,8 @@ dataset_keys = {
     
     'mexico': {
         'FEATURES_FNAME' : FEATURES_FOLDER+'/mosaiks_features_by_municipality_mexico.csv',
-        'LABELS_FNAME' : '/data/mosaiks/surveys/mexico/grouped.csv',
-        'INDIVIDUAL_FNAME' : '/data/mosaiks/surveys/mexico/',
+        'LABELS_FNAME' : SURVEY_FOLDER + 'mexico/grouped.csv',
+        'INDIVIDUAL_FNAME' : 'SURVEY_FOLDER' + 'mexico/',
         'MERGE_KEYS' : ['municipality'],
         'SPLIT_KEYS' : {'spatial':'state', 'no_spatial':'municipality'},
         'POVERTY' : 'asset_index', # Use 'asset_index' for wealth, no population density data yet
@@ -30,8 +31,8 @@ dataset_keys = {
     
     'dhs': {
         'FEATURES_FNAME' : lambda country: FEATURES_FOLDER + 'dhs/mosaiks_features_by_cluster_' + country + '.csv',
-        'LABELS_FNAME' : lambda country: '/data/mosaiks/surveys/dhs/' + country + '_grouped.csv',
-        'INDIVIDUAL_FNAME' : lambda country: '/data/mosaiks/surveys/dhs/' + country + '_hh.csv',
+        'LABELS_FNAME' : lambda country: SURVEY_FOLDER + 'dhs/' + country + '_grouped.csv',
+        'INDIVIDUAL_FNAME' : lambda country: SURVEY_FOLDER + 'dhs/' + country + '_hh.csv',
         'MERGE_KEYS' : ['cluster'],
         'SPLIT_KEYS' : {'spatial':'region', 'no_spatial':'cluster'},
         'POVERTY' : 'wealth', # Use 'wealth' for wealth, no population density data yet
@@ -42,26 +43,14 @@ dataset_keys = {
     
     'india': {
         'FEATURES_FNAME' : FEATURES_FOLDER +'mosaiks_features_by_shrug_condensed_regions_25_max_tiles_100_india.csv',
-        'LABELS_FNAME' : '/data/mosaiks/shrug/shrug_condensed_regions_25.csv',
-        'META_FNAME': '/data/mosaiks/sampled_tiles/india/shrug_condensed_regions_25_max_tiles_100_meta.csv',
+        'LABELS_FNAME' : SURVEY_FOLDER +  + 'india/' + 'grouped.csv',
+        'META_FNAME': '/data/mosaiks/replcication/sampled_tiles/india/shrug_condensed_regions_25_meta.csv',
         'INDIVIDUAL_FNAME' : None,
         'MERGE_KEYS' : ['condensed_shrug_id'],
         'SPLIT_KEYS' : {'no_spatial':'condensed_shrug_id'},
         'POVERTY' : 'secc_cons_pc_combined',
         'WEIGHT' : 'dummy_ones',
         'OUTFILE_NAME' : sim_dir+'india_condensed_regions_25_max_tiles_100/',
-        'FAIRNESS_VARS': ['rural'],
-    },
-    
-    'india-14': {
-        'FEATURES_FNAME' : FEATURES_FOLDER + 'mosaiks_features_by_shrug_condensed_regions_14_india.csv',
-        'LABELS_FNAME' : '/data/mosaiks/shrug/shrug_condensed_regions_14.csv',
-        'INDIVIDUAL_FNAME' : None,
-        'MERGE_KEYS' : ['condensed_shrug_id'],
-        'SPLIT_KEYS' : {'no_spatial':'condensed_shrug_id'},
-        'POVERTY' : 'secc_cons_pc_combined',
-        'WEIGHT' : 'dummy_ones',
-        'OUTFILE_NAME' : sim_dir+'india_condensed_regions_14/',
         'FAIRNESS_VARS': ['rural'],
     },
 
